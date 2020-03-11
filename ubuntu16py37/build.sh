bf405927f3c067156c8f5fd0dc3316a175d8c057
ricepotato <sukjun40@naver.com>
2020-03-12 01:49:01 +0900

    ubuntu16py37 version update

--- ubuntu16py37/build.sh
+++ ubuntu16py37/build.sh
@@ -1,2 +1,2 @@
-docker build -t ricepotato/ubuntu16py37:1.0.0.2 .
-docker tag ricepotato/ubuntu16py37:1.0.0.2 ricepotato/ubuntu16py37:latest
+docker build -t ricepotato/ubuntu16py37:1.0.0.3 .
+docker tag ricepotato/ubuntu16py37:1.0.0.3 ricepotato/ubuntu16py37:latest
