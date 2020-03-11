2380aa059f69edbadc6a74510a44eab50ae17bc1
ricepotato <sukjun40@naver.com>
2020-03-12 01:48:19 +0900

    update influxdb script

--- influxDB/run.sh
+++ influxDB/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+docker stop influxdb
 docker rm influxdb
 docker run -d \
 --name influxdb \
+-p 8089:8089/udp \
 -p 8086:8086 \
 -p 8083:8083 \
 -p 2003:2003  \
+--net=logger_net \
 -e INFLUXDB_GRAPHITE_ENABLED=true \
 -v /storage/influxdb/data:/var/lib/influxdb \
 -v /storage/influxdb/conf/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
