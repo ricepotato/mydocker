76b314c6c51a5d3e03504b2969187c6b595ecf54
ricepotato <sukjun40@naver.com>
2020-03-25 12:34:58 +0900

    update mockserver run script

--- mockserver/run.sh
+++ mockserver/run.sh
@@ -2,7 +2,9 @@
 
 docker stop mockserver
 docker rm mockserver
-docker run -d --rm \
--p 1080:1080 \
+docker run -d \
+-p 80:1080 \
+-p 443:1080 \
 --name mockserver \
+--restart=always \
 mockserver/mockserver -serverPort 1080 -logLevel INFO
